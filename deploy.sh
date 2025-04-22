1fc3e90339e2c49453006ca549e8a33086d4065e
Lionel Gulich <lgulich@nvidia.com>
2025-04-22 23:13:35 +0200

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-python3 -m pip install twine
-python3 setup.py sdist bdist_wheel
-twine check dist/*
-twine upload dist/*
+python3 -m pip install pipx --break-system-packages
+python3 -m pipx install build twine
+
+rm -rf dist/
+python3 -m pipx run build --sdist --wheel .
+python3 -m pipx run twine check dist/*
+python3 -m pipx run twine upload dist/*
